a8b088e23ea83a25f049e9dcc9b2abab40ae1196
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-29 21:47:19 -0300

    FEAT: a simple express server, read more...
    - add errorHandler,
    - add productRouter, with /products route.
    - parse body with express.json

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const errorHandler = require('./middlewares/errorHandler');
+const productRouter = require('./router/product');
 
 const app = express();
+
+app.use(express.json());
+
+app.use(productRouter);
+
+app.use(errorHandler);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -9,5 +17,5 @@ app.get('/', (_request, response) => {
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
