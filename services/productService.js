4232b75192e34bd34045cd10d799ea725fe2eb1b
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-30 21:11:44 -0300

    FEAT<service>: create, byId methods
    - byId gets products by id.
    - create, as it says create a product.
    - emptyData: this method checks if array is empty

--- services/productService.js
+++ services/productService.js
@@ -1,10 +1,24 @@
 const productModel = require('../models/productModel');
 
 const productService = {
+  emptyData: (data) => (data.length < 1),
   all: async () => {
     const data = await productModel.all();
+    if (productService.emptyData(data)) throw new Error('NotFoundError');
 
     return data;
+  },
+  byId: async (id) => {
+    const data = await productModel.byId(id);
+    if (productService.emptyData(data)) throw new Error('NotFoundError');
+
+    const [result] = data;
+    return result;
+  },
+  create: async (product) => {
+    const created = await productModel.create(product);
+
+    return created;
   },
 };
 
