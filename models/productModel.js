19eed136c92dd314323b8df038c4cb317d300965
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-30 21:14:09 -0300

    FEAT<MODEL>: byId and create methods

--- models/productModel.js
+++ models/productModel.js
@@ -2,10 +2,22 @@ const connection = require('../db/connection');
 
 const productModel = {
   all: async () => {
-    const findAllQuery = 'SELECT * FROM StoreManager.product;';
+    const findAllQuery = 'SELECT * FROM StoreManager.products;';
     const [data] = await connection.query(findAllQuery);
 
     return data;
+  },
+  byId: async (id) => {
+    const findByIdQuery = 'SELECT * FROM StoreManager.products WHERE id = ?;';
+    const [data] = await connection.query(findByIdQuery, [id]);
+
+    return data;
+  },
+  create: async (product) => {
+    const createProductQuery = 'INSERT INTO StoreManager.products(name) VALUES (?);';
+    const [created] = await connection.query(createProductQuery, [product]);
+
+    return created;
   },
 };
 
