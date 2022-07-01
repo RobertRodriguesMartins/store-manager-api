db41944b117258f304240c939ae9f2e087e92ba4
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-30 21:15:40 -0300

    FEAT: case structure for errors

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,18 @@
 function errorHandler(err, _req, res, _next) {
-  res.status(500).json({ message: 'algo esta errado' });
+  switch (err.message) {
+    case 'ValidationError':
+      res.status(400).json({
+        message: 'bad request',
+      });
+      return;
+    case 'NotFoundError':
+      res.status(404).json({
+        message: 'Product not found',
+      });
+      return;
+    default:
+      res.status(500).json({ message: err.message });
+  }
 }
 
 module.exports = errorHandler;
