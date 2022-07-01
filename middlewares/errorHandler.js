055f319c94419500faffe0f73f35b42003d47309
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-01 18:54:56 -0300

    FEAT<REFACTOR>:  improve code integration with joi
    - change the errorHandler object to work with joi separately

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,22 +1,31 @@
-function errorHandler(err, _req, res, _next) {
-  switch (err.message) {
-    case 'NotFoundError':
-      res.status(404).json({
-        message: 'Product not found',
-      });
-      return;
-    case '"name" is required':
-      res.status(400).json({
+function checkJoiCases(err, res) {
+  const { name } = Object.getPrototypeOf(err);
+  if (name !== 'ValidationError') return false;
+  switch (err.details[0].type) {
+    case 'any.required':
+      return res.status(400).json({
         message: err.message,
       });
-      return;
-    case '"name" length must be at least 5 characters long':
-      res.status(422).json({
+    case 'string.min':
+    case 'number.min':
+      return res.status(422).json({
         message: err.message,
       });
-      return;
     default:
-      res.status(500).json({ message: err.message });
+      return res.status(500).json({ message: err.message });
+  }
+}
+
+function errorHandler(err, _req, res, _next) {
+  if (err.message === 'NotFoundError') {
+    res.status(404).json({
+      message: 'Product not found',
+    });
+    return;
+  }
+  const isJoiError = checkJoiCases(err, res);
+  if (!isJoiError) {
+    return res.status(500).json({ message: `${err.message}-- unknown error` });
   }
 }
 
