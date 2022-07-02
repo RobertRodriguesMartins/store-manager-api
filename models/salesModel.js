a5a4b5d84303e53b7adb5090e7e3a3d86aa85f24
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-01 21:32:57 -0300

    FEAT: all method for salesModel entity

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,6 +1,18 @@
 const connection = require('../db/connection');
 
 const salesModel = {
+  all: async () => {
+    const findAllSalesQuery = `SELECT s.id saleId, s.date, sa.product_id productId,
+      sa.quantity quantity
+      FROM StoreManager.sales as s
+      INNER JOIN StoreManager.sales_products AS sa
+      ON sa.sale_id = s.id
+      ORDER BY s.id ASC, sa.product_id ASC;`;
+
+    const [data] = await connection.query(findAllSalesQuery);
+
+    return data;
+  },
   create: async () => {
     const createSaleQuery = 'INSERT INTO StoreManager.sales(`date`) values (now());';
     const created = await connection.query(createSaleQuery);
