76aed3f1a11e759f009891e9c3d242dd8c2fe77c
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-02 19:15:13 -0300

    TESTS: salesService entity 100% coverage

--- tests/unit/services/salesService.test.js
+++ tests/unit/services/salesService.test.js
@@ -9,14 +9,28 @@ chai.use(chaiPromise);
 
 describe('/services/salesService', () => {
   beforeEach(sinon.restore);
-  describe('mapProductId', () => {
-    it('should throw when arg isn"t an array', () => {
-      return chai.expect(salesService.mapProductId).to.throws();
+  describe('all', () => {
+    it('should throw when salesModel all method rejects', () => {
+      sinon.stub(salesModel, 'all').rejects();
+      return chai.expect(salesService.all()).to.eventually.be.rejected;
     });
-    it('should return an array with mapped productId', () => {
-      return chai
-        .expect(salesService.mapProductId([{ productId: 1 }]))
-        .to.be.deep.equals([1]);
+    it('should resolves with salesModel all resolved value', () => {
+      sinon.stub(salesModel, 'all').resolves(1);
+      return chai.expect(salesService.all()).to.eventually.be.deep.equals(1);
+    });
+  });
+  describe('byId', () => {
+    it('should reject when salesModel byId method rejects', () => {
+      sinon.stub(salesModel, 'byId').rejects();
+      return chai.expect(salesService.byId()).to.eventually.be.rejected;
+    });
+    it('should reject when salesModel byId return an empty array', () => {
+      sinon.stub(salesModel, 'byId').resolves([]);
+      return chai.expect(salesService.byId()).to.eventually.be.rejected;
+    });
+    it('should resolves with salesModel byId resolved value', () => {
+      sinon.stub(salesModel, 'byId').resolves([1]);
+      return chai.expect(salesService.byId()).to.eventually.be.deep.equals([1]);
     });
   });
   describe('checkProduct', () => {
@@ -78,6 +92,47 @@ describe('/services/salesService', () => {
           id: 1,
           itemsSold: [{ productId: 1, quantity: 2 }],
         });
+    });
+  });
+  describe('update', () => {
+    it('should reject when salesModel update method rejects', () => {
+      sinon.stub(salesModel, 'update').rejects();
+      sinon.stub(Promise, 'all').rejects();
+      return chai.expect(salesService.update(1, [])).to.eventually.be.rejected;
+    });
+    it('should reject if input isn"t an array', () => {
+      sinon.stub(salesModel, 'update').resolves(1);
+      return chai.expect(salesService.update(1, 2)).to.eventually.be.rejected;
+    });
+    it('should resolve with an object', () => {
+      sinon.stub(salesModel, 'update').resolves(1);
+      sinon.stub(Promise, 'all').resolves();
+      return chai
+        .expect(salesService.update(1, ['a']))
+        .to.eventually.be.deep.equals({
+          saleId: 1,
+          itemsUpdated: ['a'],
+        });
+    });
+  });
+  describe('erase', () => {
+    it('should reject when salesModel erase method rejects', () => {
+      sinon.stub(salesModel, 'erase').rejects();
+      return chai.expect(salesService.erase()).to.eventually.be.rejected;
+    });
+    it('should resolves if salesModel erase method resolves', () => {
+      sinon.stub(salesModel, 'erase').resolves(1);
+      return chai.expect(salesService.erase(1)).to.eventually.not.be.rejected;
+    });
+  });
+  describe('mapProductId', () => {
+    it('should throw when arg isn"t an array', () => {
+      return chai.expect(salesService.mapProductId).to.throws();
+    });
+    it('should return an array with mapped productId', () => {
+      return chai
+        .expect(salesService.mapProductId([{ productId: 1 }]))
+        .to.be.deep.equals([1]);
     });
   });
 });
