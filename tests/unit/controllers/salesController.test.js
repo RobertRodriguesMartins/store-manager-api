5c24ec4cfbb178c476ec1953d873ba02d5b761ae
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-02 19:16:16 -0300

    TESTS: salesController entity 60% coverage [WIP]

--- tests/unit/controllers/salesController.test.js
+++ tests/unit/controllers/salesController.test.js
@@ -19,7 +19,53 @@ describe('/controller/productController', () => {
         quantity: 2,
       },
     ],
+    params: {
+      id: 1,
+    },
   };
+  describe('all', () => {
+    it('should reject when salesService all rejects', () => {
+      sinon.stub(salesService, 'all').rejects();
+      return chai.expect(salesController.all('', response, '')).to.eventually.be
+        .rejected;
+    });
+    it('should resolves when salesService all resolves', () => {
+      sinon.stub(salesService, 'all').resolves();
+      return chai.expect(salesController.all('', response, '')).to.eventually
+        .not.be.rejected;
+    });
+    it('should call response with 200 and data', async () => {
+      sinon.stub(salesService, 'all').resolves(1);
+      await salesController.all('', response, '');
+
+      chai.expect(response.status.calledWith(200)).to.be.true;
+      chai.expect(response.json.calledWith(1)).to.be.true;
+    });
+  });
+  describe('byId', () => {
+    it('should reject when salesService byId rejects', () => {
+      sinon.stub(salesService, 'byId').rejects();
+      return chai.expect(salesController.byId(request, response, '')).to
+        .eventually.be.rejected;
+    });
+    it('should reject when joi validation fail', () => {
+      sinon.stub(salesService, 'byId').resolves();
+      return chai.expect(salesController.byId({ params: 'a' }, response, '')).to
+        .eventually.be.rejected;
+    });
+    it('should resolves when salesService byId resolves', () => {
+      sinon.stub(salesService, 'byId').resolves();
+      return chai.expect(salesController.byId(request, response, '')).to
+        .eventually.not.be.rejected;
+    });
+    it('should call response with 200 and data', async () => {
+      sinon.stub(salesService, 'byId').resolves(1);
+      await salesController.byId(request, response, '');
+
+      chai.expect(response.status.calledWith(200)).to.be.true;
+      chai.expect(response.json.calledWith(1)).to.be.true;
+    });
+  });
   describe('create', () => {
     it('should reject when joi throws', () => {
       sinon.stub(salesService, 'checkProduct').resolves();
