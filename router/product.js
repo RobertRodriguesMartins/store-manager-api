5a4a932d60a8ab16e72f630771fd6d3582a7b934
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-02 11:07:38 -0300

    FEAT: route for delete a product

--- router/product.js
+++ router/product.js
@@ -5,5 +5,6 @@ productRouter.get('/products', productController.all);
 productRouter.post('/products', productController.create);
 productRouter.get('/products/:id', productController.byId);
 productRouter.put('/products/:id', productController.update);
+productRouter.delete('/products/:id', productController.delete);
 
 module.exports = productRouter;
