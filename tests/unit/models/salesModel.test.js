8facafcb633aa4873cbaeb4c5885bd5cba897b61
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-02 19:14:14 -0300

    TESTS: salesModel entity 100% coverage

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -8,6 +8,26 @@ chai.use(chaiPromise);
 
 describe('/models/salesModel', () => {
   beforeEach(sinon.restore);
+  describe('all', () => {
+    it('should rejects when db rejects', () => {
+      sinon.stub(connection, 'query').rejects();
+      return chai.expect(salesModel.all()).to.eventually.be.rejected;
+    });
+    it('should return an array', () => {
+      sinon.stub(connection, 'query').resolves([1]);
+      return chai.expect(salesModel.all()).to.eventually.be.deep.equals(1);
+    });
+  });
+  describe('byId', () => {
+    it('should rejects when db rejects', () => {
+      sinon.stub(connection, 'query').rejects();
+      return chai.expect(salesModel.byId()).to.eventually.be.rejected;
+    });
+    it('should return an array', () => {
+      sinon.stub(connection, 'query').resolves([1]);
+      return chai.expect(salesModel.byId()).to.eventually.be.deep.equals(1);
+    });
+  });
   describe('create', () => {
     it('should rejects when db rejects', () => {
       sinon.stub(connection, 'query').rejects();
@@ -30,6 +50,26 @@ describe('/models/salesModel', () => {
       return chai
         .expect(salesModel.createUserSale())
         .to.eventually.be.deep.equals([{ insertId: 1 }]);
+    });
+  });
+  describe('erase', () => {
+    it('should rejects when db rejects', () => {
+      sinon.stub(connection, 'query').rejects();
+      return chai.expect(salesModel.erase()).to.eventually.be.rejected;
+    });
+    it('should resolves when db resolves', () => {
+      sinon.stub(connection, 'query').resolves();
+      return chai.expect(salesModel.erase()).to.eventually.not.be.rejected;
+    });
+  });
+  describe('update', () => {
+    it('should rejects when db rejects', () => {
+      sinon.stub(connection, 'query').rejects();
+      return chai.expect(salesModel.update()).to.eventually.be.rejected;
+    });
+    it('should resolves when db resolves', () => {
+      sinon.stub(connection, 'query').resolves();
+      return chai.expect(salesModel.update()).to.eventually.not.be.rejected;
     });
   });
 });
