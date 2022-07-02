a90248f304abc4e9f4599af0c79b703187c03a88
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-01 22:19:40 -0300

    FEAT: new route for sales, now u can get by id
    - byId sales route

--- router/sales.js
+++ router/sales.js
@@ -2,6 +2,7 @@ const salesRouter = require('express').Router();
 const salesController = require('../controllers/salesController');
 
 salesRouter.get('/sales', salesController.all);
+salesRouter.get('/sales/:id', salesController.byId);
 salesRouter.post('/sales', salesController.create);
 
 module.exports = salesRouter;
