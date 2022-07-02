7bf1de794c9f75245062bd23bd425f31d56f85df
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-01 21:32:10 -0300

    FEAT: new route for sales Router
    - get all sales from /sales

--- router/sales.js
+++ router/sales.js
@@ -1,6 +1,7 @@
 const salesRouter = require('express').Router();
 const salesController = require('../controllers/salesController');
 
+salesRouter.get('/sales', salesController.all);
 salesRouter.post('/sales', salesController.create);
 
 module.exports = salesRouter;
