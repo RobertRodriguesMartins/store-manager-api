b4fe8f345995aeb50a4b5bb034c78869da6f7cbf
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-02 19:14:43 -0300

    TESTS: productModel entity 100% coverage

--- tests/unit/models/productModel.test.js
+++ tests/unit/models/productModel.test.js
@@ -40,4 +40,38 @@ describe('/models/productModel', () => {
         .to.eventually.be.deep.equal([{ insertId: 1 }]);
     });
   });
+  describe('erase', () => {
+    it('should rejects when db rejects', () => {
+      sinon.stub(connection, 'query').rejects();
+      return chai.expect(productModel.erase()).to.eventually.be.rejected;
+    });
+    it('should resolve when db resolves', () => {
+      sinon.stub(connection, 'query').resolves();
+      return chai.expect(productModel.erase()).to.not.be.rejected;
+    });
+  });
+  describe('search', () => {
+    it('should rejects when db rejects', () => {
+      sinon.stub(connection, 'query').rejects();
+      return chai.expect(productModel.search()).to.eventually.be.rejected;
+    });
+    it('should resolve with [] when db resolves', () => {
+      sinon.stub(connection, 'query').resolves([true]);
+      return chai
+        .expect(productModel.search())
+        .to.eventually.be.deep.equals(true);
+    });
+  });
+  describe('update', () => {
+    it('should rejects when db rejects', () => {
+      sinon.stub(connection, 'query').rejects();
+      return chai.expect(productModel.update()).to.eventually.be.rejected;
+    });
+    it('should resolve with [] when db resolves', () => {
+      sinon.stub(connection, 'query').resolves(true);
+      return chai
+        .expect(productModel.update())
+        .to.eventually.be.deep.equals(true);
+    });
+  });
 });
