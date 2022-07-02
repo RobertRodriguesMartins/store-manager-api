3253618cf561af675e16e1f676f3cfe0b9ef9179
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-02 10:54:41 -0300

    FEAT: new route to update a product

--- router/product.js
+++ router/product.js
@@ -4,5 +4,6 @@ const productController = require('../controllers/productController');
 productRouter.get('/products', productController.all);
 productRouter.post('/products', productController.create);
 productRouter.get('/products/:id', productController.byId);
+productRouter.put('/products/:id', productController.update);
 
 module.exports = productRouter;
