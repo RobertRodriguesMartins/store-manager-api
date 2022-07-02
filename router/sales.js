34c1d34640a01d75861f6149a7778efa2f3eac6c
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-02 11:29:22 -0300

    FEAT: delete route for sales

--- router/sales.js
+++ router/sales.js
@@ -4,5 +4,6 @@ const salesController = require('../controllers/salesController');
 salesRouter.get('/sales', salesController.all);
 salesRouter.get('/sales/:id', salesController.byId);
 salesRouter.post('/sales', salesController.create);
+salesRouter.delete('/sales/:id', salesController.erase);
 
 module.exports = salesRouter;
