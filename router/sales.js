f2b1ab8ea89b8c84419e7ae93acceba842d425b2
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-02 17:44:19 -0300

    REFACTOR: sales router to work with joi middleware

--- router/sales.js
+++ router/sales.js
@@ -1,9 +1,15 @@
 const salesRouter = require('express').Router();
 const salesController = require('../controllers/salesController');
+const joiSalesProductsValidator = require('../middlewares/joiSalesProductsValidate');
 
 salesRouter.get('/sales', salesController.all);
 salesRouter.get('/sales/:id', salesController.byId);
 salesRouter.post('/sales', salesController.create);
+salesRouter.put(
+  '/sales/:id',
+  joiSalesProductsValidator,
+  salesController.update,
+);
 salesRouter.delete('/sales/:id', salesController.erase);
 
 module.exports = salesRouter;
