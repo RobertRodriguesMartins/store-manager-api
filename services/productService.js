c1a61a5965bb79a2c1c10477b6540ee2d731162e
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-01 22:20:33 -0300

    FIX: using custom NotFoundError now

--- services/productService.js
+++ services/productService.js
@@ -1,16 +1,17 @@
 const productModel = require('../models/productModel');
+const NotFoundError = require('../utils/customErrors');
 
 const productService = {
   emptyData: (data) => data.length < 1,
   all: async () => {
     const data = await productModel.all();
-    if (productService.emptyData(data)) throw new Error('NotFoundError');
+    if (productService.emptyData(data)) throw new NotFoundError('Product not found');
 
     return data;
   },
   byId: async (id) => {
     const data = await productModel.byId(id);
-    if (productService.emptyData(data)) throw new Error('NotFoundError');
+    if (productService.emptyData(data)) throw new NotFoundError('Product not found');
 
     const [result] = data;
     return result;
