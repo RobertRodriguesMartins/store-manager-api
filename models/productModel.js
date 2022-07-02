67a1ca07fbc4ab8ac61a8064e373ec174aceffdd
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-02 10:55:57 -0300

    FEAT: new update method for productModel

--- models/productModel.js
+++ models/productModel.js
@@ -19,6 +19,13 @@ const productModel = {
 
     return created;
   },
+  update: async (id, product) => {
+    const updateProductQuery = `UPDATE StoreManager.products
+    SET name = ? WHERE id = ?;`;
+    const updated = await connection.query(updateProductQuery, [product, id]);
+
+    return updated;
+  },
 };
 
 module.exports = productModel;
